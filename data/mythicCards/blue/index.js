609b4672cced80095b7d1979a2d0effc0e0eb7a3
sohodoll <kovalevvtl@gmail.com>
2022-08-23 13:52:39 +0300

    fix

--- data/mythicCards/blue/index.js
+++ data/mythicCards/blue/index.js
@@ -1,73 +1,73 @@
 const cardsData = [
   {
     id: 'blue1',
-    cardFace: './assets/mythicCards/blue/blue1.png',
+    cardFace: './assets/MythicCards/blue/blue1.png',
     difficulty: 'hard',
     color:'blue'
   },
   {
     id: 'blue2',
-    cardFace: './assets/mythicCards/blue/blue2.png',
+    cardFace: './assets/MythicCards/blue/blue2.png',
     difficulty: 'hard',
     color:'blue'
   },
   {
     id: 'blue3',
-    cardFace: './assets/mythicCards/blue/blue3.png',
+    cardFace: './assets/MythicCards/blue/blue3.png',
     difficulty: 'easy',
     color:'blue'
   },
   {
     id: 'blue4',
-    cardFace: './assets/mythicCards/blue/blue4.png',
+    cardFace: './assets/MythicCards/blue/blue4.png',
     difficulty: 'easy',
     color:'blue'
   },
   {
     id: 'blue5',
-    cardFace: './assets/mythicCards/blue/blue5.png',
+    cardFace: './assets/MythicCards/blue/blue5.png',
     difficulty: 'easy',
     color:'blue'
   },
   {
     id: 'blue6',
-    cardFace: './assets/mythicCards/blue/blue6.png',
+    cardFace: './assets/MythicCards/blue/blue6.png',
     difficulty: 'hard',
     color:'blue'
   },
   {
     id: 'blue7',
-    cardFace: './assets/mythicCards/blue/blue7.png',
+    cardFace: './assets/MythicCards/blue/blue7.png',
     difficulty: 'normal',
     color:'blue'
   },
   {
     id: 'blue8',
-    cardFace: './assets/mythicCards/blue/blue8.png',
+    cardFace: './assets/MythicCards/blue/blue8.png',
     difficulty: 'hard',
     color:'blue'
   },
   {
     id: 'blue9',
-    cardFace: './assets/mythicCards/blue/blue9.png',
+    cardFace: './assets/MythicCards/blue/blue9.png',
     difficulty: 'normal',
     color:'blue'
   },
   {
     id: 'blue10',
-    cardFace: './assets/mythicCards/blue/blue10.png',
+    cardFace: './assets/MythicCards/blue/blue10.png',
     difficulty: 'easy',
     color:'blue'
   },
   {
     id: 'blue11',
-    cardFace: './assets/mythicCards/blue/blue11.png',
+    cardFace: './assets/MythicCards/blue/blue11.png',
     difficulty: 'normal',
     color:'blue'
   },
   {
     id: 'blue12',
-    cardFace: './assets/mythicCards/blue/blue12.png',
+    cardFace: './assets/MythicCards/blue/blue12.png',
     difficulty: 'normal',
     color:'blue'
   },
